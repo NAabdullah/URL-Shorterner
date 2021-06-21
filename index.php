c56dae1c4e26a10535581dce4828fd35e206bdb7
NAabdullah <1801854@sit.singaporetech.edu.sg>
2021-06-21 22:46:18 +0800

    css update

--- index.php
+++ index.php
@@ -9,14 +9,15 @@ $db=accessDatabase();
 $shortener = new ShortenUrl($db);
 
 $shortURL_Prefix = 'http://localhost/URL-Shorterner/?e='; 
-echo 'hello';
+
 if (isset($_POST['submit'])){
-  echo 'submitted';
+
     $url=$_POST["url"];
     try{
       $randomid=$shortener->urlShorten($url);
       $shortURL = $shortURL_Prefix.$randomid;
       echo 'Short URL: '.$shortURL;
+      unset($_POST['submit']);
 
     }catch(Exception $e){
       echo $e->getMessage();
@@ -36,15 +37,26 @@ if(isset($_GET["e"])){
 ?>
 <!DOCTYPE html>
 <html>
-<body>
-  
-<h1>What's your link?</h1>
-<p>Enter your domain</p>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-    <label for="fname">URL</label><br>
-    <input type="text" id="url" name="url"><br><br>
-    <input type="submit" name="submit" id="submit" value="Submit">
-  </form>
-
-</body>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+    <body class="center">
+      <div class="container-fluid  "> 
+        <div class="row title">
+          <h1 class="title-black">WHAT'S YOUR LINK?</h1>
+        </div>
+        <div class="row title">
+          <p>Paste it!</p>
+        </div>
+        <div class="row title">
+          <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+              <input type="text" id="url" name="url"><br><br>
+              <input type="submit" name="submit" id="submit" value="Submit">
+            </form>
+        
+      </div>
+    </body>
 </html>
